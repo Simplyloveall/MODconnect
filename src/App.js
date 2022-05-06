b900e6d4f77fcd6524309adec49bc55c9e03759c
Dustin Hurtt <dustin_hurtt@outlook.com>
2022-05-06 16:00:06 -0400

    added signup component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 
+import SignUp from "./components/SignUp";
+
 function App() {
   return (
     <div>
-      <Routes></Routes>
+      <Routes>
+
+
+        
+      </Routes>
     </div>
   );
 }
