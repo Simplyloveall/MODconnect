39a25d56aef302437bae18a838404f7a3907dc7c
Oshin Chiong <oshinchiong@Oshins-MacBook-Air.local>
2022-05-06 16:00:05 -0400

    4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
+import About from "./components/AboutUs";
 
 function App() {
   return (
-    <div>
+   <div> 
+
+    <header> 
+    <nav>
+      <Link to="/">Go Home</Link>
+      <Link to="/signup"> Signup  </Link>
+      <Link to="/aboutus"> About Us  </Link>
+      <Link to="/contacts"> Contacts  </Link>
+      
+    </nav>
+     
+  </header>
+
+
+
+    
       <Routes></Routes>
     </div>
   );
