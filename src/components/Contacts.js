1e8a72fc792516030450990b4b244b72c5140fa7
Simply Loveall <max.hkre@gmail.com>
2022-05-06 16:22:27 -0400

    minor update

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 function Contacts() {
-  return <div></div>;
+  const [contacts, setContacts] = React.useState();
+
+  return (
+    <div>
+      <h3>First Name</h3>
+      <input
+        onChange={(e) => setContacts(e.target.value)}
+        name="firstName"
+        value="firstName"
+      ></input>
+    </div>
+  );
 }
 
 export default Contacts;
